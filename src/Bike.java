80a94aab317bd2bf4bd6bf7f39a0ce2cb8620460
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-19 10:40:51 +0000

    Constructors with inheritance added

--- src/Bike.java
+++ src/Bike.java
@@ -1,3 +1,16 @@
 public class Bike extends Vehicle{
-    public String handle;
+    private String handle;
+
+    public Bike(){
+        handle = "Short";
+    }
+
+    public String getHandle(){
+        return handle;
+    }
+
+    public Bike(String engine, int wheels, int seats, int fuelTank, String lights, String handle){
+        super(engine, wheels, seats, fuelTank, lights);
+        this.handle = handle;
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,7 +1,45 @@
+
+
 public class Vehicle{
     public String engine;
     public int wheels;
     public int seats;
     public int fuelTank;
     public String lights;
+
+    public Vehicle(){
+        engine = "petrol";
+        wheels = 2;
+        seats = 2;
+        fuelTank = 14;
+        lights = "LED";
+    }
+
+    public Vehicle(String engine, int wheels, int seats, int fuelTank, String lights){
+        this.engine = engine;
+        this.wheels = wheels;
+        this.seats = seats;
+        this.fuelTank = fuelTank;
+        this.lights = lights;
+    }
+
+    public String getEngine(){
+        return engine;
+    }
+
+    public int getWheels(){
+        return wheels;
+    }
+
+    public int getSeats(){
+        return seats;
+    }
+
+    public int getFuelTank(){
+        return fuelTank;
+    }
+
+    public String getLights(){
+        return lights;
+    }
 }
