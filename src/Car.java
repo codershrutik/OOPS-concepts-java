edcd2deca93cbcf12aa7fe4b82ef4d5b173a62f1
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-19 09:57:51 +0000

    Getters and setters added

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car{
+    private String doors;
+    private String engine;
+    private String driver;
+    public int speed;
+    public float test;
+    public boolean test2;
+
+    public void setDoors(String doors){
+        doors = doors;
+    }
+
+    public void getDoors(String doors){
+        doors = doors;
+    }
+}
