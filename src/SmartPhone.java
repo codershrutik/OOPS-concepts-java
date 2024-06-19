9354f27089f56656060f76f5792592bf8b65ab1c
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-19 10:21:08 +0000

    Added more

---
+++ src/SmartPhone.java
@@ -0,0 +1,15 @@
+public class SmartPhone {
+    private String brand = "Apple";
+
+    public SmartPhone(){
+        
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+}
